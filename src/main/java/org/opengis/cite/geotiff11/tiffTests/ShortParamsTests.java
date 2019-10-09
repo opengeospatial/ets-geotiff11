0d90d7cecf7ba0e75bb3732b7d91885b45dd2db4
djutras2 <dustin.jutras@gmail.com>
2019-10-09 10:07:12 -0400

    Progress on ShortParamsTests

--- src/main/java/org/opengis/cite/geotiff11/tiffTests/ShortParamsTests.java
+++ src/main/java/org/opengis/cite/geotiff11/tiffTests/ShortParamsTests.java
@@ -1,5 +1,6 @@
 package org.opengis.cite.geotiff11.tiffTests;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.opengis.cite.geotiff11.util.TiffDump;
@@ -8,6 +9,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 // https://github.com/opengeospatial/geotiff/blob/5d6ab0ba54f1ed0174901dd84240817dc9dbe011/GeoTIFF_Standard/standard/abstract_tests/TIFF_Tests/TEST_Short_Param.adoc
+
 public class ShortParamsTests extends CommonTiffMeta {
 	
 	/*
@@ -88,32 +90,93 @@ public class ShortParamsTests extends CommonTiffMeta {
 		}
 	}*/
 	
-//	GeoKey	Requirements Class
-//	0		ignore
-//	1024	GTModelTypeGeoKey
+	//  https://github.com/opengeospatial/geotiff/blob/68d8f902293ad64526889daa055892ea30f9e9ea/GeoTIFF_Standard/Detailed%20Test%20Suite/abstract_tests/Requirements_Trace_Matrix.adoc
+	//	GeoKey	Requirements Class
+	//	0		ignore
+	//	1024	GTModelTypeGeoKey
 	
-	@Test(description = "GeoKey Directory - GTModelTypeGeoKey Test")
-	public void GTModelTypeGeoKeyTest() {
-		System.out.println(keyEntrySet.indexOf(1024));
+	@Test(description = "Short Params GTModelTypeGeoKey (1024) Test")
+	public void verifyGTModelTypeGeoKey() {
+		// the GTModelTypeGeoKey SHALL have ID = 1024
+		int index = keyEntrySet.indexOf(1024);
+		
+		// a GeoTIFF file SHALL include a GTModelTypeGeoKey
+		Assert.assertTrue(index != -1);
+		
+		// process the second Short integer in the Key Entry Set
+		int type = (int) keyEntrySet.get(index + 1);
+		// process the first Short integer in the Key Entry Set
+		int geoKey = (int) keyEntrySet.get(index);
+		// process the third Short integer in the Key Entry Set
+		int keyLength = (int) keyEntrySet.get(index + 2);
+		
+		// process the fourth Short integer in the Key Entry Set
+		int value;
+		
+		if(keyLength == 1) {
+			// SET KeyValueOffset = GeoKeyDirectory + GeoKeyOffset + 6
+			value = (int) keyEntrySet.get(index+3);
+		} else {
+			// SET KeyValueOffset = GeoKeyDirectory + (KeyValueOffset * 2)
+			value = (int) keyEntrySet.get(keyLength); // TODO: verify this is a correct interpretation of the ats...
+		}
+		
+		// the GTModelTypeGeoKey SHALL have type = SHORT		
+		Assert.assertTrue(type == 0 || type == 34735);
+		
+		// the GTModelTypeGeoKey value SHALL be: ...
+		Assert.assertTrue(Arrays.asList(0, 1, 2, 3, 32767).contains(value));
+
+		// GTModelTypeGeoKey values in the range 4-32766 SHALL be reserved
+		// TODO
+		
+		// GTModelTypeGeoKey values in the range 32768-65535 SHALL be private
+		// TODO
+		
+		// if the GTModelTypeGeoKey value is 1 (Model CRS is a projected 2D CRS) then the GeoTIFF file SHALL include a ProjectedCRSGeoKey
+		// if the GTModelTypeGeoKey value is 2 (Model CRS is a geographic 2D CRS) then the GeoTIFF file SHALL include a GeodeticCRSGeoKey
+		// if the GTModelTypeGeoKey value is 3 (Model CRS is a geocentric CRS) then the GeoTIFF file SHALL include a GeodeticCRSGeoKey
+		// if the GTModelTypeGeoKey value is 32767 (user-defined) then the GTCitationGeoKey SHALL be populated
+		switch(value) {		
+			case 1:
+				Assert.assertTrue(keyEntrySet.indexOf(3072) != -1);
+				break;
+			case 2:
+			case 3:
+				Assert.assertTrue(keyEntrySet.indexOf(3072) != -1);
+				break;
+			case 32767:
+				int gTCitationGeoKeyIndex = keyEntrySet.indexOf(1026);
+				Assert.assertTrue(gTCitationGeoKeyIndex != -1);
+				String gTCitationGeoKey = ((String) directory.getTag(34737).getValues().get(0)).substring(
+						keyEntrySet.indexOf(gTCitationGeoKeyIndex + 3), 
+						keyEntrySet.indexOf(gTCitationGeoKeyIndex + 3) + keyEntrySet.indexOf(gTCitationGeoKeyIndex + 2));
+				System.out.println("shortparams:" + gTCitationGeoKey);
+				Assert.assertFalse(gTCitationGeoKey.isEmpty()); // TODO: this is pretty rough...
+				
+				break;
+		}
+
+		
 	}
 	
 	
 
 
-//	1025	GTRasterTypeGeoKey
-//	2048	GeodeticCRSGeoKey
-//	2050	GeodeticDatumGeoKey
-//	2051	PrimeMeridianGeoKey
-//	2052	UnitsGeoKey (Linear Units)
-//	2054	UnitsGeoKey (Angular Units)
-//	2056	EllipsoidGeoKey
-//	2060	UnitsGeoKey (Azimuth Units)
-//	3072	ProjectedCRSGeoKey
-//	3074	ProjectionGeoKey
-//	3075	ProjMethodGeoKey
-//	3076	UnitsGeoKey (Linear Units)
-//	4096	VerticalGeoKey
-//	4098	VerticalDatumGeoKey
-//	4099	UnitsGeoKey (Vertical Units)
+	//	1025	GTRasterTypeGeoKey
+	//	2048	GeodeticCRSGeoKey
+	//	2050	GeodeticDatumGeoKey
+	//	2051	PrimeMeridianGeoKey
+	//	2052	UnitsGeoKey (Linear Units)
+	//	2054	UnitsGeoKey (Angular Units)
+	//	2056	EllipsoidGeoKey
+	//	2060	UnitsGeoKey (Azimuth Units)
+	//	3072	ProjectedCRSGeoKey
+	//	3074	ProjectionGeoKey
+	//	3075	ProjMethodGeoKey
+	//	3076	UnitsGeoKey (Linear Units)
+	//	4096	VerticalGeoKey
+	//	4098	VerticalDatumGeoKey
+	//	4099	UnitsGeoKey (Vertical Units)
 
 }
---
+++ src/test/java/org/opengis/cite/geotiff11/tiffTests/VerifyShortParamsTests.java
@@ -0,0 +1,66 @@
+package org.opengis.cite.geotiff11.tiffTests;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.testng.ISuite;
+import org.testng.ITestContext;
+
+/**
+ * Verifies the behavior of the TiffTagsTests test class. Test stubs replace
+ * fixture constituents where appropriate.
+ */
+public class VerifyShortParamsTests {
+
+	private static final String SUBJ = "testSubject";
+	private static ITestContext testContext;
+	private static ISuite suite;
+	ShortParamsTests iut;
+
+	public VerifyShortParamsTests() {
+		// This is the code for setting up the objects for the environment.
+		// The code should be parallel with processSuiteParameters(ISuite suite) in SuiteFixtureListener.java
+		SetIUT();
+	}
+	
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		testContext = mock(ITestContext.class);
+		suite = mock(ISuite.class);
+		when(testContext.getSuite()).thenReturn(suite);
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		
+	}
+	
+	@Test
+	public void verifyGTModelTypeGeoKey() throws Exception {
+		SetIUT();
+		iut.obtainTestSubject(testContext);
+		iut.SetUpGeoKeyDirectory();
+		iut.verifyGTModelTypeGeoKey();
+	}
+	
+	public void SetIUT() {
+		// This is the code for setting up the objects for the environment.
+		// The code should be parallel with processSuiteParameters(ISuite suite) in SuiteFixtureListener.java
+		InputStream inputStream = this.getClass().getResourceAsStream("/tmp/tiffMeta.txt");
+		try {
+			when(suite.getAttribute(SUBJ)).thenReturn(IOUtils.toString(inputStream, StandardCharsets.UTF_8));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		iut = new ShortParamsTests();
+	}
+}
