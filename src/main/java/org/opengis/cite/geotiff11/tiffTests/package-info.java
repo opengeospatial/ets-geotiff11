099e23894f6634a12ab125b53c6d761089d7f65c
djutras2 <dustin.jutras@gmail.com>
2020-01-07 09:20:28 -0500

    Removed placeholder conformance classes

---
+++ src/main/java/org/opengis/cite/geotiff11/tiffTests/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Conformance Tiff Tests includes all tiff tests. The following capabilities
+ * must be supported: 
+ * <ul>
+ * <li>TODO</li>
+ * </ul>
+ *
+ * @see <a
+ * href="http://www.w3.org/TR/html5/infrastructure.html#conformance-classes">HTML5
+ * - Conformance classes</a>
+ */
+package org.opengis.cite.geotiff11.tiffTests;
